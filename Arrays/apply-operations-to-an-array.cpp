b04093f79112ece8f698fb75c353fcf64215d1bc
Priyanshi Gajiwala <priyanshigajiwala@gmail.com>
2026-01-08 18:47:47 +0530

    Implement array operations in applyOperations method

---
+++ Arrays/apply-operations-to-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0)
+            return {};
+        for(int i=0; i<n-1 ; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+        int k = 0;
+        for(int i=0 ; i<n ; i++){
+            if(nums[i] != 0){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        while(k < n){
+            nums[k++] = 0;
+        }
+        return nums;
+    }
+};
