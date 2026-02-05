8c2aa3c35e70b349a0036f8911f8197b24cd48be
Priyanshi Gajiwala <priyanshigajiwala@gmail.com>
2026-02-05 22:27:07 +0530

    Implement transformed array construction in C++

---
+++ Arrays/Transformed_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                res[i] = 0;
+                continue;
+            }
+            int steps = nums[i];
+            int idx = (i + steps % n + n) % n;
+            res[i] = nums[idx];
+
+        }
+        return res;
+    }
+};
