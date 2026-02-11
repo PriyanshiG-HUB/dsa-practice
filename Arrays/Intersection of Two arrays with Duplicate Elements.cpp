583e70a474a0652d529fa840c4b31e20edf440e3
Priyanshi Gajiwala <priyanshigajiwala@gmail.com>
2026-02-11 12:41:07 +0530

    2-pointer technique

---
+++ Arrays/Intersection of Two arrays with Duplicate Elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> intersect(vector<int>& a, vector<int>& b) {
+        // code here
+        sort(a.begin(),a.end());
+        sort(b.begin() , b.end());
+        vector<int> res;
+        int i=0 , j=0;
+        while(i<a.size() && j<b.size()){
+            if(a[i] == b[j]){
+                // avoid duplicates in result
+                if (res.empty() || res.back() != a[i])
+                    res.push_back(a[i]);
+                i++;
+                j++;
+            }
+            else if(a[i] < b[j])
+                i++;
+            else
+                j++;
+        }
+        return res;
+    }
+};
