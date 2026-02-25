416ad0dadcfd278bb6d5eb6133e325347f38b382
Priyanshi Gajiwala <priyanshigajiwala@gmail.com>
2026-02-25 22:32:17 +0530

    Sliding Window
    TC = O(n)
    SC = O(1)

---
+++ Arrays/Maximum Number of Vowels in a Substring of Given Length.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isVowel(char c){
+        return c == 'a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+    int maxVowels(string s, int k) {
+        int count = 0;
+        for(int i=0;i<k;i++){
+            if(isVowel(s[i]))
+                count++;
+        }
+        int maxc = count;
+        for(int i=k;i<s.size();i++){
+            if(isVowel(s[i]))
+                count++;
+            if(isVowel(s[i-k]))
+                count--;
+                maxc = max(maxc , count);
+        }
+        return maxc;
+    }
+};
